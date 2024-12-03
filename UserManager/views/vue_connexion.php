5e42b6b282e91da7a6aac423ba4ffbc744508cc4
GoldSenseiBoi <98558347+GoldSenseiBoi@users.noreply.github.com>
2024-12-04 00:38:18 +0100

    add 👍 change vue connect and inscrit

--- UserManager/views/vue_connexion.php
+++ UserManager/views/vue_connexion.php
@@ -1,9 +1,9 @@
 <h2>Connexion</h2>
-<form method="post">
+<form method="post" action="traitement_connexion.php">
     <label for="email">Email :</label>
     <input type="email" name="email" id="email" required>
     <label for="password">Mot de passe :</label>
     <input type="password" name="password" id="password" required>
-    <button type="submit" name="connexion">Se connecter</button>
+    <button type="submit">Se connecter</button>
 </form>
 <p>Pas de compte ? <a href="index.php?page=inscription">Inscrivez-vous ici</a></p>
--- UserManager/views/vue_inscription.php
+++ UserManager/views/vue_inscription.php
@@ -1,5 +1,5 @@
 <h2>Inscription</h2>
-<form method="post">
+<form method="post" action="traitement_inscription.php">
     <label for="nom">Nom:</label>
     <input type="text" name="nom" required>
     <label for="prenom">Prénom:</label>
@@ -7,13 +7,13 @@
     <label for="email">Email:</label>
     <input type="email" name="email" required>
     <label for="adresse">Addresse:</label>
-    <input type="text" name="adresse" required>
+    <input type="adresse" name="adresse" required>
     <label for="code_postale">Code Postale:</label>
-    <input type="text" name="code_postale" required>
+    <input type="code_postale" name="code_postale" required>
     <label for="city">Ville:</label>
-    <input type="text" name="city" required>
+    <input type="city" name="city" required>
     <label for="password">Mot de passe:</label>
     <input type="password" name="password" required>
-    <button type="submit" name="inscription">S'inscrire</button>
+    <button type="submit">S'inscrire</button>
 </form>
 <p>Déjà un compte ? <a href="index.php?page=connexion">Connectez-vous ici</a></p>
